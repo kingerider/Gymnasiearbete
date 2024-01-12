d8cf33550db670ffee40fb3b4c2e57e7025aac82
RikJarl <erilar05@hotmail.com>
2024-01-12 14:33:50 +0100

    databas och objekt
    utökade databasen och började på objekt

---
+++ Hemsida/my_server/routes/objects.py
@@ -0,0 +1,15 @@
+class Player:
+    def __init__(self) -> None:
+        self.name = None
+        self.position = None
+
+
+class Field:
+    def __init__(self, id) -> None:
+        self.level_id = id
+    
+    def load_from_database():
+        pass
+
+    def place_player(player):
+        pass
