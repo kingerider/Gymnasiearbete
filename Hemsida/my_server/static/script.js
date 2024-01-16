ec7fb1a6663dc1ea43e223fa1d034a7c686dea54
kingerider <93842251+kingerider@users.noreply.github.com>
2024-01-16 14:41:56 +0100

    Start game

--- Hemsida/my_server/static/script.js
+++ Hemsida/my_server/static/script.js
@@ -11,3 +11,18 @@ $.ajax({
         
     }
 });
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.interval = setInterval(updateGameArea, 20);
+        },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
