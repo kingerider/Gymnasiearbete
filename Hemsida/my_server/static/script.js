45d92941fc1d74ccd5825a11ff8a75ea5810692c
kingerider <93842251+kingerider@users.noreply.github.com>
2024-01-22 18:35:14 +0100

    Bounce ball

--- Hemsida/my_server/static/script.js
+++ Hemsida/my_server/static/script.js
@@ -70,15 +70,39 @@ $.ajax({
 });
 
 //Game Code ==>
+//https://developer.mozilla.org/en-US/docs/Games/Tutorials/2D_Breakout_game_pure_JavaScript/Create_the_Canvas_and_draw_on_it
 
 function startGame() {
     console.log("Here");
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext("2d");
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+    let dx = 2;
+    let dy = -2;
+    const ballRadius = 10;
 
-    ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+      
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+    
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+        }
+        if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+    
+        x += dx;
+        y += dy;
+    }
+    setInterval(draw, 10);
 }
