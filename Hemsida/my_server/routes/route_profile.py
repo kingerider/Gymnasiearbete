e6af060a20b659e2339bfb343c2437870bbe2120
kingerider <93842251+kingerider@users.noreply.github.com>
2024-04-11 21:40:32 +0200

    Profile

--- Hemsida/my_server/routes/route_profile.py
+++ Hemsida/my_server/routes/route_profile.py
@@ -2,7 +2,7 @@ from my_server import app
 from flask import render_template, session
 from my_server.routes.dbhandler import create_connection
 from my_server.routes.route_login import user_logged_in
-
+import json
 @app.route('/profile')
 def profile():
     user_logged_in()
@@ -10,7 +10,33 @@ def profile():
     cur = conn.cursor()
     inlogged_user = cur.execute("SELECT * FROM user WHERE id = ?", (session['id'], )).fetchone()
     user_levels = cur.execute("SELECT * FROM level WHERE creator_id = ?", (session['id'], )).fetchall()
+
+    list_user_levels_data = []
+    for level in user_levels:
+        level_id = level[0]
+        wall_x = cur.execute("SELECT x_coordinate FROM wall WHERE level_id == ?", (level[0], )).fetchall()
+        wall_x = [x[0] for x in wall_x]
+        wall_y = cur.execute("SELECT y_coordinate FROM wall WHERE level_id == ?", (level[0], )).fetchall()
+        wall_y = [y[0] for y in wall_y]
+        monster_x = cur.execute("SELECT x_coordinate FROM enemy WHERE level_id == ?", (level[0], )).fetchall()
+        monster_x = [x[0] for x in monster_x]
+        monster_y = cur.execute("SELECT y_coordinate FROM enemy WHERE level_id == ?", (level[0], )).fetchall()
+        monster_y = [y[0] for y in monster_y]
+        title = cur.execute("SELECT title FROM level WHERE id == ?", (level[0], )).fetchone()[0]
+        description = cur.execute("SELECT description FROM level WHERE id == ?", (level[0], )).fetchone()[0]
+        hearts = cur.execute("SELECT player_health FROM level WHERE id == ?", (level[0], )).fetchone()[0]
+        user_levels_data = {
+            'levelId': level_id,
+            'wallX': wall_x,
+            'wallY': wall_y,
+            'monsterX': monster_x,
+            'monsterY': monster_y,
+            'title': title,
+            'description': description,
+            'hearts': hearts
+        }
+        list_user_levels_data.append(user_levels_data)
+    print(list_user_levels_data)
     conn.close()
-    print(inlogged_user)
-    print(user_levels)
-    return render_template('profile.html', user = inlogged_user, levels = user_levels)
+
+    return render_template('profile.html', user = inlogged_user, levels = user_levels, levels_data=json.dumps(list_user_levels_data))
---
+++ Hemsida/my_server/static/profile.mjs
@@ -0,0 +1,71 @@
+$("levels_data").text()
+import { Position, drawPlayer, drawMonster, drawWall, drawGrid } from './draw_module.mjs';
+
+function initializeCanvasNew(htmlId, levelId, data) {
+    JSON.stringify(data)
+
+    //Canvas
+    var canvas = document.getElementById(htmlId);
+    var ctx = canvas.getContext("2d");
+    var tileSize = canvas.height / 20
+    var canvasHeight = canvas.height / tileSize
+    var canvasWidth = canvas.width / tileSize
+
+    //Player
+    var playerHeight = tileSize;
+    var playerWidth = tileSize;
+    var playerArray = [];
+    playerArray.push(new Position(parseInt(canvasWidth) / 8, parseInt(canvasHeight) / 2));
+    playerArray.push(new Position(parseInt(canvasWidth) - parseInt(canvasWidth) / 8 - 1, parseInt(canvasHeight) / 2));
+
+    //Wall
+    var wallHeight = tileSize;
+    var wallWidth = tileSize;
+    var wallArray = [];
+
+    //Monster
+    var monsterHeight = tileSize;
+    var monsterWidth = tileSize;
+    var monsterArray = [];
+
+    for (const level_data in data) {
+        if (Object.hasOwnProperty.call(data, level_data)) {
+            const level_data_dictionary = data[level_data];
+            if (levelId == level_data_dictionary['levelId']){
+                setVariables(level_data_dictionary)
+            }
+        }
+    }
+   
+    function setVariables(data) {
+        for (let index = 0; index < data['wallX'].length; index++) {
+            wallArray.push(new Position(data['wallX'][index], data['wallY'][index]));
+        }
+        for (let index = 0; index < data['monsterX'].length; index++) {
+            monsterArray.push(new Position(data['monsterX'][index], data['monsterY'][index]));
+        }
+        $("#edit_title").val(data['title'])
+        $("#edit_description").val(data['description'])
+
+    }
+
+    //Draw objects
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid(canvas.width, canvas.height, tileSize, ctx);
+        for (let i = 0; i < wallArray.length; i++) {
+            drawWall(wallArray[i].getX() * tileSize, wallArray[i].getY() * tileSize, wallWidth, wallHeight, ctx);
+        }
+        for (let i = 0; i < monsterArray.length; i++) {
+            drawMonster(monsterArray[i].getX() * tileSize, monsterArray[i].getY() * tileSize, monsterWidth, monsterHeight, ctx);
+        }
+        for (let i = 0; i < playerArray.length; i++) {
+            drawPlayer(playerArray[i].getX() * tileSize, playerArray[i].getY() * tileSize, playerWidth, playerHeight, ctx);
+        }
+    }
+    let updateInterval = setInterval(draw, 100);
+    setTimeout(function () { clearInterval(updateInterval); }, 1000); //Kill interval for efficent server resources
+
+}
+
+export { initializeCanvasNew }
